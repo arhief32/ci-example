8d1993354d8cf9c2306709053454394433ac0a0a
Arief Muarrief Buhari <arhief32@gmail.com>
2018-08-01 17:42:48 +0700

    create login form

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -25,4 +25,46 @@ class Admin extends CI_Controller
         $this->AdminModel->register($register_data);
         $this->load->view('auth/login');
     }
+
+    function login_page()
+    {
+        $data['status'] = '';
+        $this->load->view('auth/login', $data);
+    }
+
+    function login()
+    {
+        $login_data = [
+            'username' => $this->input->post('username'),
+            'password' => md5($this->input->post('password'))
+        ];
+
+        $check_login = $this->AdminModel->login($login_data)->num_rows();
+        $result = $this->AdminModel->login($login_data)->result();
+
+        if($check_login > 0)
+        {
+            $data_session = array(
+				'name' => $result[0]->full_name,
+				'status' => "login"
+				);
+ 
+            $this->session->set_userdata($data_session);
+
+            var_dump('berhasil uuuyeeeaah');
+
+			// redirect(base_url('dashboard'));
+        }
+        else
+        {
+            $data['status'] = "your username and password don't match. try again";
+            
+            $this->load->view('auth/login', $data);
+        }
+    }
+
+    function logout()
+    {
+
+    }
 }
